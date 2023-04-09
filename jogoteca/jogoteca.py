faa962f48800564d9d6618af6a9861d36a5c14ed
Bruno <brunocosta.1991@hotmail.com>
2023-04-09 18:27:33 -0300

    list and for to view list jogos

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    lista = ['Tetris', 'Skyrim', 'Crash']
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
 app.run(host='0.0.0.0', port=8080)
