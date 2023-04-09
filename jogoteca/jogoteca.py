dfeaf5decd845f8d95062038a4af33b25aa4adf1
Bruno <brunocosta.1991@hotmail.com>
2023-04-09 18:43:24 -0300

    new route to add games into list

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
 
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    lista = ['Tetris', 'Skyrim', 'Crash']
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of war', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
 
 
 app.run(host='0.0.0.0', port=8080)
